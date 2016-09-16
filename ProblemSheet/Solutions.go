1d62c870aa39e9cc861badfbeb4c1ec847e02268
Kieran Whyte <G00254624@gmit.ie>
2016-09-16 20:17:56 +0100

    Commit 4

--- ProblemSheet/Solutions.go
+++ ProblemSheet/Solutions.go
@@ -111,6 +111,105 @@ func main(){
 
 	fmt.Println("All done")*/
 	//END SOLUTION TO PROBLEM 4
+
+	//SOLUTION TO PROBLEM 5
+	var fourNotEntered bool = true
+	var word string
+	var letterChangeCnt int = 1
+	var loopControlCnt int = 1
+	var mainSwitchCnt int = 0
+	var permutations = make([]string, 0)
+	var resultWord string
+	var loop = true
+
+	//Keep asking user for a four letter word until one is entered
+	for fourNotEntered {
+		fmt.Print("Enter 4 letter word: ")
+		fmt.Scanln(&word)
+
+		if(len(word) == 4){
+			fourNotEntered = false
+		}
+	}
+
+	//stored word will be used each time we start of. We can make each 6 combinations from each letter
+	//So for example if word starts with 'f' we can make 6 combinations and keep f at the start 6x4=24
+	//The program will start with a letter then switch the remaining three letters after it 6 times.
+
+	//Steps
+	//1 Store original word
+	//2 Get the last three characters
+	//3 Switch them six times, 1st-2nd, 1st-3rd, 1st-2nd etc ..each time appending them onto the starting letter
+	//4 After six switches we grab the original word again and switch 1st-2nd letters
+	//5 repeat steps 2 and 3
+	//6 Again after the six switches grab original word, this time switch 1st-3rd letters
+	//7 repeat steps 2 and 3
+	//8 Again after the six switches grab original word, this time switch 1st-4th letters
+	//9 repeat steps 2 and 3
+	//All Done :)
+	//(Each word is appended to a string slice as we go along)
+
+	storedWord := []rune(word)
+	lastThreeLetters := storedWord[1:4]
+	firstLetter := storedWord[0:1]
+
+	for loop {
+
+		if letterChangeCnt == 1 {
+			permutations = append(permutations, string(storedWord))
+		} else {
+
+			if letterChangeCnt % 2 == 0{
+
+				lastThreeLetters = reverseFirstTwoLetters(string(lastThreeLetters))
+				resultWord = string(firstLetter)
+				resultWord += string(lastThreeLetters)
+				permutations = append(permutations, resultWord)
+
+			} else if letterChangeCnt % 2 == 1 {
+				lastThreeLetters = reverseFirstAndThirdLetters(string(lastThreeLetters))
+				resultWord = string(firstLetter)
+				resultWord += string(lastThreeLetters)
+				permutations = append(permutations, resultWord)
+			}
+
+		}
+
+		if letterChangeCnt == 6 && mainSwitchCnt == 0 {
+			storedWord = reverseFirstTwoLetters(string(storedWord))
+			lastThreeLetters = storedWord[1:4]
+			firstLetter = storedWord[0:1]
+
+			mainSwitchCnt++
+			letterChangeCnt = 0
+		} else if letterChangeCnt == 6 && mainSwitchCnt == 1 {
+			storedWord = reverseFirstAndThirdLetters(string(storedWord))
+			lastThreeLetters = storedWord[1:4]
+			firstLetter = storedWord[0:1]
+
+			mainSwitchCnt++
+			letterChangeCnt = 0
+		} else if letterChangeCnt == 6 && mainSwitchCnt == 2 {
+			storedWord = reverseFirstAndFourthLetters(string(storedWord))
+			lastThreeLetters = storedWord[1:4]
+			firstLetter = storedWord[0:1]
+
+			letterChangeCnt = 0
+		}
+
+		letterChangeCnt++
+		loopControlCnt++
+
+		if loopControlCnt == 25 {
+			loop = false
+		}
+	}
+
+	fmt.Printf("Here are all combinations of the word '%s'", word)
+	fmt.Println()
+	fmt.Println(permutations)
+
+	//END SOLUTION TO PROBLEM 5
 }
 
 //reverse function used to solve problem 3
@@ -129,3 +228,38 @@ func main(){
 	return string(result)
 }*/
 
+func reverseFirstTwoLetters(word string) []rune {
+
+	var switcher rune
+	data := []rune(word)
+
+	switcher = data[0]
+	data[0] = data[1]
+	data[1] = switcher
+
+	return data
+}
+
+func reverseFirstAndThirdLetters(word string) []rune {
+
+	var switcher rune
+	data := []rune(word)
+
+	switcher = data[0]
+	data[0] = data[2]
+	data[2] = switcher
+
+	return data
+}
+
+func reverseFirstAndFourthLetters(word string) []rune {
+
+	var switcher rune
+	data := []rune(word)
+
+	switcher = data[0]
+	data[0] = data[3]
+	data[3] = switcher
+
+	return data
+}
